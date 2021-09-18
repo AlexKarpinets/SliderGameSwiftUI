66407718f26b3f4b4797b872f2e85fc39f197cf7
AlexKarpinets <66379289+AlexKarpinets@users.noreply.github.com>
2021-09-18 14:27:47 +0300

    Initial Commit

---
+++ SliderGameSwiftUI/SliderGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameSwiftUIApp.swift
+//  SliderGameSwiftUI
+//
+//  Created by Karpinets Alexander on 18.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
